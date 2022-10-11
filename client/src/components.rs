b07a84bcee2e3c75be394b7fcd0eceafae6b7997
TheSeaToad <me@theseatoad.com>
2022-10-10 23:44:30 -0700

    added logic for animations

--- client/src/components.rs
+++ client/src/components.rs
@@ -8,3 +8,40 @@ pub struct Player;
 
 #[derive(Component)]
 pub struct Position(pub Vec2);
+
+#[derive(Component)]
+pub struct Direction(pub EDirection);
+
+#[derive(Debug, PartialEq)]
+pub enum EDirection {
+    UP,
+    LEFT,
+    DOWN,
+    RIGHT
+}
+
+impl Default for EDirection {
+    fn default() -> EDirection {
+        EDirection::RIGHT
+    }
+}
+
+#[derive(Component)]
+pub struct CharacterState(pub ECharacterState);
+
+
+#[derive(Debug, PartialEq)]
+pub enum ECharacterState {
+    IDLE,
+    ATTACK,
+    DEAD
+}
+
+impl Default for ECharacterState {
+    fn default() -> ECharacterState {
+        ECharacterState::IDLE
+    }
+}
+
+#[derive(Component, Deref, DerefMut)]
+pub struct AnimationTimer(pub Timer);
--- client/src/player.rs
+++ client/src/player.rs
@@ -1,7 +1,7 @@
-use bevy::{prelude::*, time::FixedTimestep, sprite::Anchor};
+use bevy::{prelude::*, time::FixedTimestep};
 
 use crate::{
-    components::{Player, Position},
+    components::{AnimationTimer, CharacterState, Direction, ECharacterState, EDirection, Player},
     TIME_STEP,
 };
 pub struct PlayerPlugin;
@@ -13,55 +13,157 @@ impl Plugin for PlayerPlugin {
         app.add_startup_system(init_player).add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
-                .with_system(move_player),
+                .with_system(move_player)
+                .with_system(animate_sprite),
         );
     }
 }
 
-fn init_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn init_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
     let _loaded = asset_server.load_folder("game/characters/archer");
+    //Note that every other is empty
+    let texture_handle = asset_server.get_handle("game/characters/archer/spritesheet.png");
+    let texture_atlas = TextureAtlas::from_grid_with_padding(
+        texture_handle,
+        Vec2::new(10.0, 10.0),
+        33,
+        1,
+        Vec2::new(0.0, 0.),
+        Vec2::new(5.0, 5.0),
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands
-        .spawn_bundle(SpriteBundle {
-            texture: asset_server.get_handle("game/characters/archer/wee_mons_archer_idle_d_1.png"),
-            transform: Transform {
-                translation: Vec3 {
-                    x: 0.0,
-                    y: 0.0,
-                    z: 10.0,
-                },
-                scale : Vec3 { x: 2., y: 2., z: 2. },
-                ..Default::default()
-            },
-            sprite: Sprite {
-                anchor: Anchor::BottomLeft,
-                ..Default::default()
-            },
-            ..Default::default()
+        .spawn_bundle(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(Vec3::splat(2.0)),
+            ..default()
         })
-        .insert(Player);
+        .insert(Player)
+        .insert(Direction(EDirection::default()))
+        .insert(CharacterState(ECharacterState::default()))
+        .insert(AnimationTimer(Timer::from_seconds(0.1, true)));
 }
 
-fn move_player(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Transform, With<Player>>, time: Res<Time>) {
-    let mut transform = query.get_single_mut().expect("Could not find player in move_player");
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut Direction, &mut CharacterState), With<Player>>,
+    time: Res<Time>,
+) {
+    let (mut transform, mut direction, mut character_state) = query
+        .get_single_mut()
+        .expect("Could not find player in move_player");
     let mut move_input: Vec2 = Vec2::ZERO;
     if keyboard_input.pressed(KeyCode::W) || keyboard_input.just_released(KeyCode::Up) {
         move_input.y = 1.;
+        direction.0 = EDirection::UP;
     }
     if keyboard_input.pressed(KeyCode::S) || keyboard_input.just_released(KeyCode::Down) {
         move_input.y = -1.;
+        direction.0 = EDirection::DOWN;
     }
     if keyboard_input.pressed(KeyCode::A) || keyboard_input.just_released(KeyCode::Left) {
         move_input.x = -1.;
+        direction.0 = EDirection::LEFT;
     }
     if keyboard_input.pressed(KeyCode::D) || keyboard_input.just_released(KeyCode::Right) {
         move_input.x = 1.;
+        direction.0 = EDirection::RIGHT;
     }
-
     // Arena bounds
     // x: -210, 210
     // y : -160, 120,
-    let mut new_player_position = transform.translation + move_input.extend(0.) * PLAYERSPEED * time.delta_seconds();
+    let mut new_player_position =
+        transform.translation + move_input.extend(0.) * PLAYERSPEED * time.delta_seconds();
     new_player_position.x = new_player_position.x.clamp(-220., 185.);
     new_player_position.y = new_player_position.y.clamp(-180., 100.);
     transform.translation = new_player_position;
+}
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<
+        (
+            &mut AnimationTimer,
+            &mut TextureAtlasSprite,
+            &crate::components::Direction,
+            &CharacterState,
+        ),
+        With<Player>,
+    >,
+) {
+    for (mut timer, mut sprite, direction, character_state) in &mut query {
+        println!(
+            "Direction : {:?} and state : {:?}",
+            direction.0, character_state.0
+        );
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            match direction.0 {
+                EDirection::LEFT => {
+                    if character_state.0 == ECharacterState::IDLE {
+                        if sprite.index == 0 {
+                            sprite.index = 2;
+                        } else {
+                            sprite.index = 0;
+                        }
+                    } else if character_state.0 == ECharacterState::ATTACK {
+                        if sprite.index == 0 {
+                            sprite.index = 2;
+                        } else {
+                            sprite.index = 0;
+                        }
+                    };
+                }
+                EDirection::RIGHT => {
+                    if character_state.0 == ECharacterState::IDLE {
+                        if sprite.index == 0 {
+                            sprite.index = 2;
+                        } else {
+                            sprite.index = 0;
+                        }
+                    } else if character_state.0 == ECharacterState::ATTACK {
+                        if sprite.index == 0 {
+                            sprite.index = 2;
+                        } else {
+                            sprite.index = 0;
+                        }
+                    };
+                }
+                EDirection::UP => {
+                    if character_state.0 == ECharacterState::IDLE {
+                        if sprite.index == 0 {
+                            sprite.index = 2;
+                        } else {
+                            sprite.index = 0;
+                        }
+                    } else if character_state.0 == ECharacterState::ATTACK {
+                        if sprite.index == 0 {
+                            sprite.index = 2;
+                        } else {
+                            sprite.index = 0;
+                        }
+                    };
+                }
+                EDirection::DOWN => {
+                    if character_state.0 == ECharacterState::IDLE {
+                        if sprite.index == 0 {
+                            sprite.index = 2;
+                        } else {
+                            sprite.index = 0;
+                        }
+                    } else if character_state.0 == ECharacterState::ATTACK {
+                        if sprite.index == 0 {
+                            sprite.index = 2;
+                        } else {
+                            sprite.index = 0;
+                        }
+                    };
+                }
+            };
+        }
+    }
 }
